ec0ceab02c749834801291095e406ef8272510be
EpicFrog2000 <107147908+EpicFrog2000@users.noreply.github.com>
2023-02-23 21:30:27 +0100

    DAY3 update
    Linked funkcja.cpp

--- pathing.cpp
+++ pathing.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <map>
 #include <mysql/mysql.h>
+#include "funkcja_v2.cpp"
 using namespace std;
 int main(){
     /*
@@ -21,6 +22,7 @@ int main(){
     MYSQL *db;
     db = mysql_real_connect(connection, "localhost", "norbert", "", "pociag_v3", 0, NULL, 0);
     const char *query;
+    /*
     string zapytanie = "SELECT `Nazwa_stacji`,`Poz_x`,`Poz_y` FROM stacje;"; // query text
     query = zapytanie.c_str();
     mysql_query(connection, query);
@@ -35,5 +37,19 @@ int main(){
     for(const auto& elem : map){
         cout << "Miasto: " <<elem.first << " Pozycja x: " << elem.second.first << " Pozycja y: " << elem.second.second << "\n";
     }
+    */
+    string zapytanie = "SELECT `Nazwa_stacji`,`Poz_x`,`Poz_y` FROM stacje WHERE Nazwa_stacji in ('Kraków','Warszawa');"; //Wybierz miasto
+    query = zapytanie.c_str();
+    mysql_query(connection, query);
+    res = mysql_store_result(connection);
+	while (row = mysql_fetch_row(res)){
+        pair<string,string> value(row[1],row[2]);
+        map[row[0]] = value;
+	}
+    mysql_close(connection);
+    pair<double, double> Q = make_pair(stoi(map["Warszawa"].first, nullptr, 10), stoi(map["Warszawa"].second, nullptr, 10));
+    pair<double, double> P = make_pair(stoi(map["Kraków"].first, nullptr, 10), stoi(map["Kraków"].second, nullptr, 10));
+    getLineDirection(P, Q); // Wypisuje wzór prostej lini miedzy dwoma miastami oraz x,y przez ktore ta linia przechodzi
+
     return 0;
 }
